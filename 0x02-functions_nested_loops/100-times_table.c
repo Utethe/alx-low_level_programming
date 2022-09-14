633af8d6171e417b090b5e0a3d9ef1b73add5c50
Utethe <sutethe@gmail.com>
2022-09-14 09:38:41 -0700

    task 100 and 101

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,51 +1,18 @@
 #include "main.h"
+
 /**
-* print_times_table - Prints a multiplication table up to param
-* @n: The number to be treated
-*Programming
-* Return: Number matrix
-*/
-void print_times_table(int n)
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
-	int x, y, z;
-
-	if (n >= 0 && n <= 14)
-	{
-	for (x = 0; x <= n; x++)
-		{
-			for (y = 0; y <= n; y++)
-			{
-				z = x * y;
-				if (z > 99)
-				{
-					_putchar(',');
-					_putchar(32);
-					_putchar((z / 100) + '0');
-					_putchar(((z / 10) % 10) + '0');
-					_putchar((z % 10) + '0');
-				}
-				else if (z > 9)
-				{
-					_putchar(',');
-					_putchar(32);
-					_putchar(32);
-					_putchar(((z / 10) % 10) + '0');
-					_putchar((z % 10) + '0');
-				}
-				else
-				{
-					if (y != 0)
-					{
-						_putchar(',');
-						_putchar(32);
-						_putchar(32);
-						_putchar(32);
-					}
-					_putchar(z + '0');
-				}
-			}
-			_putchar('\n');
-		}
-	}
+	print_times_table(3);
+	_putchar('\n');
+	print_times_table(5);
+	_putchar('\n');
+	print_times_table(98);
+	_putchar('\n');
+	print_times_table(12);  
+	return (0);
 }
-
