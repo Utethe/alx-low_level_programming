78c98439ea15089e840c35d6d211f9a3cd88d97a
Utethe <sutethe@gmail.com>
2022-09-15 07:12:50 -0700

    100 and 104

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,50 +1,87 @@
+#include <stdlib.h>
 #include "main.h"
+
 /**
-* print_times_table - Prints a multiplication table up to param
-* @n: The number to be treated
-*Programming
-* Return: Number matrix
-*/
+ * printm - helper function, print integer using _putchar
+ * @n: number to print
+ */
+void printm(long n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10)
+		printm(n / 10);
+	_putchar(n % 10 + '0');
+}
+
+/**
+ * countn - helper function, counts the number of digits in a number
+ * @n: number to count
+ * Return: number of digits in n
+ */
+int countn(long n)
+{
+	int l;
+
+	if (n >= 0 && n <= 9)
+	{
+		return (1);
+	}
+	l = !n;
+	while (n)
+	{
+		l++;
+		n /= 10;
+	}
+	return (l);
+}
+
+/**
+ * print_times_table - prints the n times table, starting with 0.
+ * @n: times table stop
+ * Return: void
+ */
 void print_times_table(int n)
 {
-	int x, y, z;
+	int i, k, p, nd, l;
 
-	if (n >= 0 && n <= 14)
+	if (n > 15 || n < 0)
 	{
-	for (x = 0; x <= n; x++)
+		return;
+	}
+	if (n == 0)
+	{
+		printm(n);
+		_putchar('\n');
+		return;
+	}
+	for (i = 0; i <= n; i++)
+	{
+		for (k = 0; k <= n; k++)
 		{
-			for (y = 0; y <= n; y++)
+			p = i * k;
+			if (k == 0)
 			{
-				z = x * y;
-				if (z > 99)
+				printm(p);
+				_putchar(',');
+			}
+			else
+			{
+				nd = countn(p);
+				for (l = 1; l <= 4 - nd; l++)
+				{
+					_putchar(' ');
+				}
+				printm(p);
+				if (k != n)
 				{
 					_putchar(',');
-					_putchar(32);
-					_putchar((z / 100) + '0');
-					_putchar(((z / 10) % 10) + '0');
-					_putchar((z % 10) + '0');
-				}
-				else if (z > 9)
-				{
-					_putchar(',');
-					_putchar(32);
-					_putchar(32);
-					_putchar(((z / 10) % 10) + '0');
-					_putchar((z % 10) + '0');
-				}
-				else
-				{
-					if (y != 0)
-					{
-						_putchar(',');
-						_putchar(32);
-						_putchar(32);
-						_putchar(32);
-					}
-					_putchar(z + '0');
 				}
 			}
-			_putchar('\n');
 		}
+		_putchar('\n');
 	}
 }
